0ccc887c91b94be15f1d3d5f81051aa5d630a742
mukesh-kumar-sdei <mukesh.kumar@smartdatainc.net>
2022-10-19 10:24:46 +0530

    Renamed folder

--- Chatfe-Project/Pods/Target Support Files/Pods-Chatfe-ChatfeUITests/Pods-Chatfe-ChatfeUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Chatfe_ChatfeUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Chatfe_ChatfeUITests
-@end
--- Chatfe-Project/Pods/Target Support Files/ReactionButton/ReactionButton-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_ReactionButton : NSObject
-@end
-@implementation PodsDummy_ReactionButton
-@end
